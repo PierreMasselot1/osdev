ce712735e00b12d37eca122f8e56c05aa2ccc23c
PierreMasselot1 <pierrem@orohealth.me>
2024-05-12 17:24:29 -0400

    basic hello world example working

---
+++ src/kernel.cpp
@@ -0,0 +1,13 @@
+#include "io.hpp"
+
+int main(){
+    const char *helloMessage = "Hello, World!\n";
+    uart_init();
+    uart_writeText(helloMessage);
+
+    while(1){
+        ;
+    }
+
+    return 0;
+}
